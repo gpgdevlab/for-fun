c63c85363a4d17aee3872c09214d9bc5ee4fc61e
Pimentel <79454863+gpgdevlab@users.noreply.github.com>
2026-05-29 20:12:28 -0300

    Add update and personalized greeting functions
    Added a new function 'atualizar' to update item values in the dictionary and a 'personalizada' function for user input.

--- 290526.py
+++ 290526.py
@@ -15,13 +15,40 @@ def adicionar(dicionario):
 
     return print(f"{item} foi adicionado com o valor R${valor} no dicionário")
 
-klinck ={
+def atualizar(dicionario): 
+    print(f"Qual item você quer atualizar o valor?")
 
+    for key in dicionario:
+        print(key)
+
+    print("Caso deseje retornar digite *Sair*")
+
+    item = str(input())   
+
+    while item != "Sair":
+        if item not in dicionario:
+            print("Não achamos o item no estoque")
+        else:
+            valor = int(input("Qual o novo valor do item?"))
+            dicionario[item] = valor
+            print(f"{item} tem um novo preço agora")
+        
+    return 0
+
+klinck ={
+    "k" : 34,
+    "j" : 34,
+    "l" : 34,
+    "v" : 34
 }
 
 
 lista = [1, "dois", 4]
 
-inverter(lista)
 
-adicionar(klinck)
+def personalizada():
+    nome = str(input(""))
+    idade = int(input(""))
+    return print(f"Olá, {nome} você possui {idade} anos")
+
+atualizar(klinck)
